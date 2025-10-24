92a7afe6f0ee44cab7f4099bec7271898589dfeb
nelelizalde24 <elizaldejnjesus@outlook.com>
2025-10-24 09:58:57 -0600

    Prueba de escritorio del ejercicio de dianostico_medico.pl

---
+++ Prolog/dianostico_medico.pl
@@ -0,0 +1,63 @@
+
+enfermedad(gripe).
+enfermedad(rubeola).
+enfermedad(malaria).
+enfermedad(hepatitis).
+enfermedad(tuberculosis).
+enfermedad(anemia).
+
+
+sintomade(tos, gripe).
+sintomade(cansancio, gripe). 
+sintomade(fiebre, gripe). 
+sintomade(dolorcabeza, gripe). 
+sintomade(nauseas, hepatitis). 
+sintomade(diarrea, hepatitis). 
+sintomade(ictericia, hepatitis). 
+sintomade(cansancio, anemia).
+sintomade(apatia, anemia).
+sintomade(nausea, anemia).
+sintomade(tos, tuberculosis).
+sintomade(cansancio, tuberculosis).
+sintomade(fiebre, tuberculosis). 
+sintomade(escalofrios, tuberculosis).
+sintomade(escalofrios, malaria).
+sintomade(fiebre , malaria). 
+sintomade(diarrea , malaria).
+sintomade(ictericia, malaria).
+sintomade(fiebre, rubeola).
+sintomade(jaqueca, rubeola). 
+sintomade(secrecion, rubeola).
+
+
+
+buscar([], E , 0).
+buscar(X , E , 1) :- sintomade(X, E).
+buscar([X|Xs] , E , P) :- enfermedad(E) , buscar(X, E , S1) , buscar(Xs , E ,S2) , P is S1 + S2.
+
+cantSint(E , C) :- findall(X , sintoma(X, E) , L) ,length(L , R), C is R.
+diagnostico([X|Xs] , E , K) :- buscar([X|Xs] , E ,P) , cantSint(E , T) , K is P * 100 / T.
+
+
+medicinade(contrex, gripe). 
+medicinade(jarabe, gripe). 
+medicinade(pastillas, tubercolosis). 
+medicinade(vacuna, malaria). 
+medicinade(vacuna, rubéola). 
+medicinade(vitaminas, anemia). 
+medicinade(pastillas, hepatitis). 
+
+recetade(M, S):-sintomade(S, Z),medicinade(M, Z).
+especialistade(otorrino, gripe).
+especialistade(nutricionista, anemia).
+especialistade(endocrinologia, hepatitis).
+especialistade(medicinageneral, rubéola).
+especialistade(nutricionista, tubercolosis).
+especialistade(medicinageneral, malaria).
+
+
+atiende_especialista(E, S):- sintoma_de(S,Z),especialista_de(E, Z).
+
+mereceta(Es, M, E):-medicinade(M, E),sintomade(S, E), atiendeespecialista(Es,S).
+
+
--- Prolog/listas.pl
+++ Prolog/listas.pl
@@ -15,3 +15,28 @@ longitud([],0).
 longitud([X|Y],N):- longitud(Y,N1), N is N1+1.
 
 concatenar(X, Y, R):- append(X,Y,R).
+
+
+invertir([], []).
+invertir([X|T], R) :- invertir(T, RT), append(RT, [X], R).
+
+
+ultimo([X], X).
+ultimo([_|T], X) :-ultimo(T, X).
+
+
+suma_lista([], 0).
+suma_lista([X|T], S) :-suma_lista(T, ST),S is X + ST.
+
+
+eliminar(X, [X|T], T).
+eliminar(X, [Y|T], [Y|R]) :- X \= Y, eliminar(X, T, R).
+
+
+duplicar([], []).
+duplicar([X|T], [X,X|R]) :- duplicar(T, R).
+
+
+intercalar([], L, L).
+intercalar(L, [], L).
+intercalar([X|Xs], [Y|Ys], [X,Y|R]) :- intercalar(Xs, Ys, R).
